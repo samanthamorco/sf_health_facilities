081687ac407de3bd4fddd6b50c03404bb574e12e
samanthamorco <morkotiks@gmail.com>
2016-06-06 20:46:28 -0700

    updated the gem to add a json file

--- lib/sf_health_facilities.rb
+++ lib/sf_health_facilities.rb
@@ -1,5 +1,23 @@
 require "sf_health_facilities/version"
 
 module SfHealthFacilities
-  # Your code goes here...
+  class Facilities
+
+    attr_reader :facility_name, :facility_type, :location_address
+
+    def initialize(hash)
+      @facility_name = hash["facility_name"]
+      @facility_type = hash["facility_type"]
+      @location_address = hash["location_address"]
+    end
+
+    def self.all
+      facility_array = Unirest.get("https://data.sfgov.org/resource/sci7-7q9i.json").body
+      facilities = []
+      facility_array.each do |facility_hash|
+        facilities << Facilities.new(facility_hash)
+      end
+      return facilities
+    end
+  end
 end
